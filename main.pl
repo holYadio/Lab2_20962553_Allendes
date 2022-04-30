7ce958463a980171228e60a76e2ae9365bc0e97e
Jhoisan Allendes <jhoisan.allendes@usach.cl>
2022-04-30 16:43:31 -0400

    Se complementa el trabajo en el constructor

--- main.pl
+++ main.pl
@@ -23,8 +23,12 @@ myRandom(Xn, Xn1):-
 	Xn1 is (AXC mod 2147483647).
 
 
+cardsSet(_,0,_,_,[]).
+%cardsSet(Elements, NumElementos, MaxC, Seed, [Card|RestCard]):-
+%	not(NumElementos = 0),
+	
+
 generaListaRandoms(0, _, _, []).
-cardsSet(Elements, NumElementos, MaxC, Seed, []).
 generaListaRandoms(Cont, Xi, Hasta, [PrimerNumero|LR2]):-
 	not(Cont = 0),
 	myRandom(Xi, Xi1),
@@ -32,9 +36,42 @@ generaListaRandoms(Cont, Xi, Hasta, [PrimerNumero|LR2]):-
 	generaListaRandoms(ContMenos1, Xi1, Hasta, LR2),
 	PrimerNumero is Xi1 mod Hasta.
 
+%generarCarta(Elements,CantElement,[PrimerElemento|LAux]):-
+	%not(CantElement = 0),
+	%myRandom() 
 
+
+
+% Calcula longuitud de la lista
+long([],0).
+long([_|Y],S):-long(Y,T), S is T + 1.
+
+generarFirstCard(_,0,[]).
+
+generarFirstCard(ListaElement,CantElement,N,[Element|Carta]):-
+	not(CantElement = 0),
+	CantElementMenos1 is CantElement -1,
+	M is N + 1,
+	generarFirstCard(ListaElement,CantElementMenos1,M,Carta),
+	Element is elementoN(ListaElement,N,).
+
+%generarFirstCard([PrimerElemento|RestoElementos],CantElement,[Element|Carta]):-
+	%not(CantElement = 0),
+	%CantElementMenos1 is CantElement -1,
+	%generarFirstCard(RestoElementos,CantElementMenos1,Carta),
+	%Element is PrimerElemento.
+
+
+elementoN([],0,_):-!.
+elementoN([X|_],0,X).
+elementoN([_|Y],Cont,U):-
+	not(Cont = 0),
+	ContMenos1 is Cont -1,
+	elementoN(Y,ContMenos1,U).
 
 % Carta enesima
-cardsSetNthCard([],N,[]):-!.
-cardsSetNthCard[X|N],1,X):-!.
-cardsSetNthCard([X|R],N,S):-M is N - 1, cardsSetNthCard(R,M,S).
+%cardsSetNthCard([],N,[]):-!.
+%cardsSetNthCard[X|N],1,X):-!.
+%cardsSetNthCard([X|R],N,S):-
+	%M is N - 1,
+	%cardsSetNthCard(R,M,S).
